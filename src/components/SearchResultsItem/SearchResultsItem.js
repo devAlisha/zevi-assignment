a00c5685757e1a6cc319f5410ce78a4d056bc9e5
Alisha Singh <alishasingh4546@gmail.com>
2023-12-26 17:36:48 +0530

    add link in search Results Item

--- src/components/SearchResultsItem/SearchResultsItem.js
+++ src/components/SearchResultsItem/SearchResultsItem.js
@@ -1,9 +1,15 @@
 import { Box, Text } from "@chakra-ui/react";
-import './SearchResultsItem.scss';
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink, LinkProps } from "@chakra-ui/react";
+import "./SearchResultsItem.scss";
 export default function SearchResultsItem({ product }) {
   return (
-      <Box className="search-results-item">
-        <Text className="search-results-item-name">{product.name}</Text>
-      </Box>
+    <Box className="search-results-item">
+      <ChakraLink as={ReactRouterLink} to="/products" _hover={{ textDecoration: 'none' }}>
+        <Text className="search-results-item-name">
+          {product.name}
+        </Text>
+      </ChakraLink>
+    </Box>
   );
 }
